d33b44d1bc22c0e44b194cb15dc2e3776baafe88
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-16 12:23:37 +0300

    tardeproje

---
+++ danisman/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { ReactComponent as Harita } from "./vectormap2.svg";
+// import harita from "./vectormap2.svg";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    <div className="map-of-regions">
+      <ul className="list-of-regions">
+        <li data-state="Gaziantep" className="region-card on">
+          <div className="region-image">
+          {/* <img src={harita} alt="Şehir" className="rounded-start"></img> */}
+          </div>
+          <div className="region-content">
+            <div className="fs-6 title">Şehir İsmi</div>
+            <div className="desc">
+              <p>Lorem ipsum.</p>
+            </div>
+          </div>
+        </li>
+        ...
+      </ul>
+      <Harita></Harita>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ danisman/src/Components/Header.jsx
@@ -0,0 +1,7 @@
+const Header =()=>{
+    return(
+        <>Header Bölümü</>
+    )
+}
+
+export default Header
