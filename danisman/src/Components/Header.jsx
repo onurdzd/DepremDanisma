eaa0cc60e571d77decdb48a49eab75bcc72fc299
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-17 11:38:06 +0300

    dashboard update

--- danisman/src/Components/Header.jsx
+++ danisman/src/Components/Header.jsx
@@ -12,19 +12,7 @@ function classNames(...classes) {
 const Header = () => {
   return (
     <>
-
-      <div className="text-xl w-screen bg-white">
-        <nav className="flex justify-around ">
-          <div className=" text-blue-300">
-            <a href={"/"}>TARDE</a>
-          </div>
-          <div className=" text-red-300">
             <Login></Login>
-          </div>
-        </nav>
-      </div>
-    
-
 
 
       <header className="text-xl w-screen bg-white mx-auto ">
