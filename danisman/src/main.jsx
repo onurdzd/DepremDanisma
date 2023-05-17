f192d4a1ddfe172d8cad67b85a5b44eff3c55017
Mrtgk93 <mrtgk93@hotmail.com>
2023-05-17 12:33:09 +0300

    dashboard'a header eklendi

--- danisman/src/main.jsx
+++ danisman/src/main.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
-import Dashboard from './Components/Dashboard.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Dashboard from "./Components/Dashboard.jsx";
+import Header from "./Components/Header.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div><App /></div>,
+    element: (
+      <div>
+        <App />
+      </div>
+    ),
   },
   {
     path: "/dashboard",
-    element: <div><Dashboard /></div>,
+    element: (
+      <div className="h-screen">
+        <Header />
+        <Dashboard />
+      </div>
+    ),
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
