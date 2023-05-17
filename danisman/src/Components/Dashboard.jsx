505b1688bba3c62fbf899e690f52b9767bd3d3a9
Mrtgk93 <mrtgk93@hotmail.com>
2023-05-17 09:51:09 +0300

    dashboard güncelleme

--- danisman/src/Components/Dashboard.jsx
+++ danisman/src/Components/Dashboard.jsx
@@ -1,20 +1,20 @@
-
 const Dashboard = () => {
   return (
-    <div>
-    <div className="text-xl w-screen justify bg-white mt-20">
-      <nav className="flex justify-around ">
-        <div className=" text-blue-300">
-        <a href={"/"}>TARDE</a>
-        </div>
+    <div className="flex flex-col gap-11 pt-5 h-screen w-screen ">
+      <div className="text-xl   bg-white ">
+        <nav className="flex justify-around ">
+          <div className=" text-blue-300">
+            <a href={"/"}>TARDE</a>
+          </div>
+          <div className=" text-red-300 ">
+            <button>
+              <a href={"/"}>Ana Sayfa</a>
+            </button>
+          </div>
+        </nav>
+      </div>
+      <div className=" flex  justify-evenly">
         <div className=" text-red-300 ">
-          <button>
-            <a href={"/"}>Ana Sayfa</a>
-          </button>
-        </div>
-      </nav>
-    </div>
-    <div className=" text-red-300 ">
           <button>
             <a href={"/danisan"}>Danışan Ekle</a>
           </button>
@@ -34,6 +34,7 @@ const Dashboard = () => {
             <a href={"/bilgiguncelle"}>Bilgi Güncelle</a>
           </button>
         </div>
+      </div>
     </div>
   );
 };
