ddfe8ff0f751de8d7893bae5f3b2c5d956c4d4c2
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-22 15:02:15 +0300

    personel name ile search

--- backend/api/personel/personel-router.js
+++ backend/api/personel/personel-router.js
@@ -2,20 +2,28 @@ const router = require("express").Router();
 const Personels = require("./personel-model");
 
 router.get("/", async (req, res, next) => {
-    try {
-      const personels = await Personels.getAll();
-      res.status(201).json(personels);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const personels = await Personels.getAll();
+    res.status(201).json(personels);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/:id", mwuser.idIsValid, async (req, res, next) => {
-    try {
-      res.status(201).json(req.personel);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    res.status(201).json(req.personel);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports=router
+router.get("/:name", mwuser.nameIsValid, async (req, res, next) => {
+  try {
+    res.status(201).json(req.personel);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
