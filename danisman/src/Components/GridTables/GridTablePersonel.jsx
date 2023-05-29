677ff1ce0e747027a48e7d5f336073089d515c42
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-29 14:50:28 +0300

    envanter tablosu güncellendi

--- danisman/src/Components/GridTables/GridTablePersonel.jsx
+++ danisman/src/Components/GridTables/GridTablePersonel.jsx
@@ -379,7 +379,7 @@ const GridTablePersonel = () => {
             <Menu as="div" className="relative inline-block text-left">
               <div>
                 <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                  Merkez Seç
+                {newRowData.merkez_id ? "Merkez Seçildi" : "Merkez Seç"}
                   <ChevronDownIcon
                     className="-mr-1 h-5 w-5 text-gray-400"
                     aria-hidden="true"
