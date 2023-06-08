6af7c83a987687ee539e7c513bb7152598d80816
Mrtgk93 <mrtgk93@hotmail.com>
2023-06-08 12:22:37 +0300

    hakkımızda kısmında tasarım değişiklikleri yapıldı ama responsive'e göre düzenlenecek

--- danisman/src/Components/Hakkimizda.jsx
+++ danisman/src/Components/Hakkimizda.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { MdPeopleAlt, MdPerson } from "react-icons/md";
 import { useState, useEffect } from "react";
-import SagUst from "../../public/sagust.svg"
-import SagOrta from "../../public/sagorta.png"
+import SagUst from "../../public/sagust.svg";
+import SagOrta from "../../public/sagorta.png";
 
 function Hakkimizda() {
   const [hizmetData, setHizmetData] = useState([]);
@@ -20,25 +20,38 @@ function Hakkimizda() {
   );
 
   return (
-   <>
-      <h1 className="pt-10 text-6xl text-blue-950 font-bold block ml-10 h-full">hakkımızda</h1>
+    <>
+      <h1 className="pt-10 text-6xl text-blue-950 font-bold block ml-10 h-full">
+        hakkımızda
+      </h1>
       <div className="w-full flex flex-col items-center">
-      <div className="flex flex-col items-center w-full">
-        <div className="flex justify-evenly"><p className="font-thin mt-4 text-[32px] leading-[45px] w-[50%] pt-10">
-        Affan kötülükten uzak durandır. Besleyicidir, Bağımsızdır, Çeşitlidir, Barışseverdir, Kapsayıcıdır
-      </p><img src={SagUst} className=""></img></div>
-      <div className="flex justify-evenly w-full">
-      <p className="font-light mt-4 text-sm leading-6 w-[50%]">
-      Affan, Travma ve Afet Ruh Sağlığı Çalışmaları Derneği (TARDE) bünyesinde Kahramanmaraş Pazarcık merkezli deprem sonrası Maraş, Hatay, Antep, İskenderun, Osmaniye, Diyarbakır, Urfa, Adıyaman, Malatya’daki merkezlerde depremden etkilenen bireylere yönelik ücretsiz psikolojik destek hizmeti sunan, İstanbul Bilgi Üniversitesi Travma ve Afet Ruh Sağlığı Çalışmaları Uygulamalı Ruh Sağlığı Programı tarafından desteklenen psikososyal destek hizmeti projesidir.
-      </p>
-   <img src={SagOrta}></img>
+        <div className="flex flex-col items-center w-full">
+          <div className="flex justify-between ml-[6.6rem] mr-[3rem]">
+            <p className="font-thin mt-4 text-[32px] leading-[45px] w-[55%] pt-10">
+              Affan kötülükten uzak durandır. Besleyicidir, Bağımsızdır,
+              Çeşitlidir, Barışseverdir, Kapsayıcıdır
+            </p>
+            <img src={SagUst} className=" w-44 mt-[-8rem]"></img>
+          </div>
+          <div className="flex justify-between ml-[6.6rem] mb-14 mr-[3rem]">
+            <p className="font-light mt-14 text-sm leading-6 w-[48%]">
+              Affan, Travma ve Afet Ruh Sağlığı Çalışmaları Derneği (TARDE)
+              bünyesinde Kahramanmaraş Pazarcık merkezli deprem sonrası Maraş,
+              Hatay, Antep, İskenderun, Osmaniye, Diyarbakır, Urfa, Adıyaman,
+              Malatya’daki merkezlerde depremden etkilenen bireylere yönelik
+              ücretsiz psikolojik destek hizmeti sunan, İstanbul Bilgi
+              Üniversitesi Travma ve Afet Ruh Sağlığı Çalışmaları Uygulamalı Ruh
+              Sağlığı Programı tarafından desteklenen psikososyal destek hizmeti
+              projesidir.
+            </p>
+            <img src={SagOrta} className="h-52"></img>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      <div className="flex flex-col mt-3  mr-6 flex-grow  rounded-3xl  ">
-        <div className=" flex flex-row mt-14 pb-3 justify-between mx-6">
-          <div className="font-sans  flex  font-semibold text-[4rem] bg-white px-6 py-1 rounded-xl ">
-            <div className="flex  flex-col mr-10 ml-2 ">
+      <div className="flex flex-col mt-1  mr-6 flex-grow  rounded-3xl  ">
+        <div className=" flex flex-row mt-[-2rem] pb-12 justify-between ml-[5rem]">
+          <div className="font-sans flex  font-semibold text-[4rem] bg-white px-6 py-1 h-[11rem] rounded-xl ">
+            <div className="flex flex-col mr-10 ml-2  ">
               <img
                 className="max-w-[9rem] pt-2 pb-2 px-6 ml-4 "
                 src="logo.jpg"
@@ -75,18 +88,18 @@ function Hakkimizda() {
               </div>
             </div>
           </div>
-        <div className="bg-amber-300 bg-opacity-30 w-[20%] h-[20%] mt-[-5rem]  mr-18 rounded-3xl">
-            <button className="mt-2 mx-3 px-6 py-2 bg-amber-300 rounded-xl inline-block text-xs font-bold text-blue-900">
+          <div className="bg-amber-300  bg-opacity-30 w-[14rem] h-[15.8rem] mt-[-1.5rem]  rounded-3xl">
+            <button className="mt-6 w-[12.4rem] mx-3 px-6 py-2 bg-amber-300 rounded-xl inline-block text-xs font-bold text-blue-900">
               GÖNÜLLÜ OL
             </button>
             <img
-              className="max-h-[25vh] py-3 min-h-full min-w-full"
+              className="max-h-[15rem] py-3 min-h-full min-w-full"
               src="ilust.png"
             />
           </div>
         </div>
       </div>
-      </>
+    </>
   );
 }
 
