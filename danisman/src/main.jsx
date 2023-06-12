0c8262ed5742f45f0dcb7e3f1a0beed85c7b7646
onurd <106928484+onurdzd@users.noreply.github.com>
2023-06-12 15:00:47 +0300

    hashrouter deneme

--- danisman/src/main.jsx
+++ danisman/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import Dashboard from "./Components/Dashboard.jsx";
@@ -21,7 +21,7 @@ import Hakkimizda from "./Components/Hakkimizda.jsx";
 import Form from "./Components/Destekbasvuru.jsx";
 import GonulluOl from "./Components/GonulluOl.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: (
