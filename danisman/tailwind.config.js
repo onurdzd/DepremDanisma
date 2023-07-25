0317728b73f596e6ca667cd4598fff78d5ca5f57
Gökhan <gn.ozdemir@gmail.com>
2023-07-25 15:47:59 +0300

    Add Default fonts

--- danisman/tailwind.config.js
+++ danisman/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Roboto", "ui-sans-serif", "system-ui"],
+      },
+      colors: {
+        "theme-blue": "#243c5a",
+        "theme-yellow": "#F6BE31",
+        "theme-dark": "#000C5C",
+      },
+    },
   },
   plugins: [],
-}
-
+};
